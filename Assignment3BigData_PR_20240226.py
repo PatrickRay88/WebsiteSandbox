30c9c1e2ff932e10e02e1a021c93e82a8cc3fcf7
Patrick Ray <patrick.d.ray.88@gmail.com>
2024-02-26 14:02:03 -0500

    added comments

--- Assignment3BigData_PR_20240226.py
+++ Assignment3BigData_PR_20240226.py
@@ -15,7 +15,7 @@ key = "somekey"
 ######################################
 #      API from rentcast.io
 ######################################
-   
+   ####TESTING COMMIT ANDS UPDATEs
 def API_call():
 
     url = "https://api.rentcast.io/v1/avm/value?address=5500%20Grand%20Lake%20Drive%2C%20San%20Antonio%2C%20TX%2C%2078244&propertyType=Single%20Family&bedrooms=4&bathrooms=2&squareFootage=1600&compCount=20"
